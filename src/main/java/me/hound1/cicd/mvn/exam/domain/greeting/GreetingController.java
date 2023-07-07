af6f43b5c79672e73907e1ed44620e79e40bcb51
Hounds1 <zoodisx@gmail.com>
2023-07-07 17:15:12 +0900

    Test github webhook

---
+++ src/main/java/me/hound1/cicd/mvn/exam/domain/greeting/GreetingController.java
@@ -0,0 +1,13 @@
+package me.hound1.cicd.mvn.exam.domain.greeting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String index() {
+        return "/index";
+    }
+}
