b597b8309e7b28b1f9c0d0d1660a5353f7083859
Hounds1 <zoodisx@gmail.com>
2023-07-07 16:56:29 +0900

    git init

---
+++ src/main/java/me/hound1/cicd/mvn/exam/ExamApplication.java
@@ -0,0 +1,13 @@
+package me.hound1.cicd.mvn.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamApplication.class, args);
+	}
+
+}
